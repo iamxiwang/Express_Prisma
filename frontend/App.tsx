26cfb7104d006e26a6d9f785419bf6f71fc9b65a
XiWang <vivian.xi.wang@gmail.com>
2023-06-22 22:55:50 -0700

    added JokeItem

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import JokeItem from './components/JokeItem';
 
 export default function App() {
   return (
     <SafeAreaView  className="flex-1 items-center justify-center bg-white">
       <StatusBar/>
-      <Text>Open up n your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up your app!</Text>
+      <JokeItem />
     </SafeAreaView>
   );
 }
---
+++ frontend/components/JokeItem.tsx
@@ -0,0 +1,14 @@
+import { View, Text,Pressable } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const JokeItem = (props: Props) => {
+    return (
+        <View className="Rectangle1 w-[113px] h-[140px] bg-gradient-to-tl from-cyan-500 to-cyan-500 rounded-xl shadow" style={{ backgroundColor: '#00B5AD' }}>
+
+        </View>
+    )
+}
+
+export default JokeItem
