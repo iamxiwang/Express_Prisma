36a1f975928c086e5d304a2c542c8696c144f032
XiWang <vivian.xi.wang@gmail.com>
2023-07-16 22:33:51 -0700

    make a login page html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup';
-
+import Login from './components/Login'
 function App() {
   return (
     <Routes>
         <Route path ='/' element={<Signup />} ></Route>
+        <Route path ='/login' element={<Login />} ></Route>
     </Routes>
   );
 }
