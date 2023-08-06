907aafd74b59e99180353403ae6b0b77fa9f8b61
XiWang <vivian.xi.wang@gmail.com>
2023-08-06 12:17:44 -0700

    added register routes for user

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
 
-
+const bcrypt = require('bcryptjs');
 
 
 router.get('/', async (req, res, next) => {
@@ -12,6 +12,74 @@ router.get('/', async (req, res, next) => {
     }catch (error) {
         next(error)
     }
+});
+
+// POST /api/users/register
+router.post('/register', async (req, res, next) => {
+    //  Check to make sure no one has already registered with the proposed email or username.
+        const existingUser = await prisma.user.findFirst({
+            where: {
+                OR: [
+                    { email: req.body.email },
+                    { firstName: req.body.firstName, lastName: req.body.lastName }
+                ]
+            }
+        });
+
+        if (existingUser) {
+            // Throw a 400 error if the email address and/or username already exists
+            const err = new Error("Validation Error");
+            err.statusCode = 400;
+            const errors = {};
+            if (existingUser.email === req.body.email) {
+                errors.email = "A user has already registered with this email";
+            }
+            if (
+                existingUser.firstName === req.body.firstName &&
+                existingUser.lastName === req.body.lastName
+            ) {
+                errors.name = "A user has already registered with this name";
+            }
+            
+            err.errors = errors;
+            return next(err);
+        }
+            // If the query fails to find anyone, then you are good to create and save the new user in the database! (For now, just assume that the input is valid; you will add validations in a few phases.)
+
+            // Create a new User with the given email and username. Don't include the password: you don't want to store the password in plain text! Instead, use bcrypt to create a salted and encrypted password hash that can be stored in the new user's hashedPassword field. Then save the user to the database and return it as JSON. (Once you set up Passport, you will want to log in the newly created user as well.)
+            
+            try {
+                    // Generate a salt
+                    bcrypt.genSalt(10, async (err, salt) => {
+                    if (err) throw err;
+                
+                    // Hash the password using the generated salt
+                    bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+                        if (err) throw err;
+                
+                        try {
+                        // Create a new user with hashedPassword
+                        const newUser = await prisma.user.create({
+                            data: {
+                                email: req.body.email,
+                                firstName: req.body.firstName,
+                                lastName: req.body.lastName,
+                                hashedPassword: hashedPassword
+                            }
+                        });
+                
+                        // Return the newly created user
+                        res.json({ user: newUser });
+                        } catch (error) {
+                        next(error);
+                        }
+                    });
+                    });
+                } catch (error) {
+                    next(error);
+                }
+
+    
 });
 
 
