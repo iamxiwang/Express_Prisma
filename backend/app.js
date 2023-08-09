99c12d8ccbf05aed29954de61f53c3946225fe8b
XiWang <vivian.xi.wang@gmail.com>
2023-08-09 11:42:11 -0700

    add csrf token to all non get routes

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,42 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
+const debug = require('debug')
 const morgan = require('morgan');
 require('dotenv').config();
 
-require('./config/passport'); // <-- ADD THIS LINE
-const passport = require('passport'); // <-- ADD THIS LINE
+require('./config/passport'); 
+const passport = require('passport'); 
 
+//Express server secrity
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+app.use(cookieParser());
+
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, the Express 
+  // server will serve the React files statically.)
+  app.use(cors());
+}
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 
 app.get('/', async (req, res, next) => {
   res.send({ message: 'Awesome it works 🐻' });
@@ -21,11 +47,22 @@ app.use('/api', require('./routes/jokes.route'));
 // user routes
 const usersRouter = require('./routes/users.route');
 app.use('/api/users', usersRouter);
-
+//csrf routes
+const csrfRouter = require('./routes/csrf');
+app.use('/api/csrf', csrfRouter);
 
 app.use((req, res, next) => {
   next(createError.NotFound());
 });
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
