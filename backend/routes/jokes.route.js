6f51b8a02b5b9a4f499295d91d78f26a5649b886
XiWang <vivian.xi.wang@gmail.com>
2023-09-21 18:02:23 -0700

    test redis in local and add google Oauth in sign in

--- backend/routes/jokes.route.js
+++ backend/routes/jokes.route.js
@@ -1,14 +1,27 @@
 const router = require('express').Router();
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
+const Redis = require('ioredis'); // Import the Redis library
 
+const redisClient = new Redis({
+  host: 'localhost', // or '127.0.0.1' if Redis is running locally
+  port: 6379, // default Redis port
 
-
+})              
 
 router.get('/jokes', async (req, res, next) => {
     try{
+        const cachedJokes = await redisClient.get('jokes')
+        if(cachedJokes){
+          const jokes = JSON.parse(cachedJokes)
+          return res.json(jokes)
+        }
+        // If data is not cached, fetch it from your database (Prisma)
         const jokes = await prisma.joke.findMany({})
+        // Store the data in Redis with an expiration time (e.g., 1 hour)
+        await redisClient.set('jokes', JSON.stringify(jokes), 'ex', 3600);
         res.json(jokes)
+
     }catch (error) {
       next(error)
     }
